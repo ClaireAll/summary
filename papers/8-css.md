049d928ba94852a60caac8fe1e3db72727f0fd5d
ClaireAll <1119518763@qq.com>
2023-02-23 15:14:13 +0800

    按钮

--- papers/8-css.md
+++ papers/8-css.md
@@ -1,2 +1,36 @@
  ### 响应式图片
  使用两个新的属性——srcset 和 sizes——来提供更多额外的资源图像和提示
+
+ ### 重设所有属性
+ CSS 的简写属性 all 可以用于同时将这些继承值中的一个应用于（几乎）所有属性。
+
+ ### 伪元素
+ + ::after
+ + ::before
+ + ::first-letter
+ + ::first-line
+ + ::grammar-error
+ + ::selection 只能定义元素被选中时的 color、background、cursor、outline 以及 text-shadow
+ + ::spelling-error
+
+### 背景
++ background-repeat: no-repeat/repeat-x/repeat-y/repeat
++ background-size: cover/contain
++ background-position: x y
++ background-attachment: scroll/fixed/local
+
+### 书写模式
++ writing-mode: horizontal-tb/vertical-rl/vertical-lr
+
+### 相对长度单位
+|单位|相对于
+|:-|:-:|
+|em|在 font-size 中使用是相对于父元素的字体大小，在其他属性中使用是相对于自身的字体大小，如 width
+|ex|字符“x”的高度
+|ch|数字“0”的宽度
+|rem|根元素的字体大小
+|1h|元素的line-height
+|vw|视窗宽度的1%
+|vh|视窗高度的1%
+|vmin|视窗较小尺寸的1%
+|vmax|视图大尺寸的1%
--- vue-claire/src/views/css/CssTest.vue
+++ vue-claire/src/views/css/CssTest.vue
@@ -50,7 +50,14 @@
                     </table>
                 </el-card>
             </el-col>
-            <el-col :span="7"></el-col>
+            <el-col :span="7">
+                <el-card class="box-card cool-button">
+                    <template #header>
+                        <h3>一个按钮</h3>
+                    </template>
+                    <p>A Cool Button</p>
+                </el-card>
+            </el-col>
         </el-row>
         <el-row>
             <el-col :span="17">
@@ -129,7 +136,21 @@
                     </table>
                 </el-card>
             </el-col>
-            <el-col :span="4"></el-col>
+            <el-col :span="6">
+                <el-card class="box-card selection">
+                    <template #header>
+                        <h3>::selection伪元素</h3>
+                    </template>
+                    <p>
+                        霜天晓角·晚次东阿
+                        <br />
+                        朱彝尊清代
+                        <br />
+                        鞭影匆匆，又铜城驿东。<br />过雨碧罗天净，才八月，响初鸿。
+                        微风何寺钟？夕曛岚翠重。<br />十里鱼山断处，留一抹、枣林红。
+                    </p>
+                </el-card>
+            </el-col>
         </el-row>
         <el-row>
             <el-col :span="17">
@@ -289,6 +310,52 @@ div {
 
     tr:nth-child(odd) {
         background-color: rgb(245, 245, 245);
+    }
+}
+.selection {
+    p {
+        -webkit-animation-name: scaleWord;
+        -webkit-animation-timing-function: ease-in-out;
+        -webkit-animation-iteration-count: infinite;
+        -webkit-animation-duration: 2s;
+    }
+
+    p::selection {
+        background-color: pink;
+        color: purple;
+        text-shadow: 1px 2px 2px tomato;
+    }
+
+    @keyframes scaleWord {
+        0% {
+            transform: scale(1);
+        }
+        50% {
+            transform: scale(1.2);
+        }
+        100% {
+            transform: scale(1);
+        }
+    }
+}
+
+.cool-button p {
+    width: 200px;
+    height: 100px;
+    line-height: 100px;
+    text-align: center;
+    font-size: 20px;
+    font-weight: bold;
+    color: white;
+    text-shadow: 2px 2px rgb(84, 84, 84);
+    background: linear-gradient(#e66465, #9198e5);
+    box-sizing: border-box;
+    border-radius: 10px;
+    box-shadow: 2px 2px 4px rgb(36, 36, 36), inset 2px 2px 4px white, inset -2px -2px 4px rgb(36, 36, 36);
+    &:hover {
+        background: linear-gradient(#ff9898, #bcc2ff);
+        cursor: pointer;
+        text-shadow: 2px 2px #676ca0;
     }
 }
 </style>
